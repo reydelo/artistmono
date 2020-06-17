d24470a06e352ff8eabc0c91ff4df1b76b7a62c6
Reyna D <reyna.deloge@gmail.com>
2020-06-17 22:49:35 +0200

    quick start mono page with images

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,27 @@
-import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-const Header = ({ siteTitle }) => (
+import MonoLogo from '../images/favicon.jpg';
+
+const Header = () => (
   <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
   >
     <div
       style={{
         margin: `0 auto`,
-        maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+      <img src={MonoLogo}
+        style={{
+          maxWidth: 100,
+          borderRadius: 50,
+          marginBottom: 0
+        }}
+      />
     </div>
   </header>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,20 +2,24 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+
+import NeckTat from '../images/neck-tat.jpg'
+import FlorenciasLegs from '../images/florencias-legs.jpg'
+import InasLipoma from '../images/inas-lipoma.jpg';
+import LegsOfADancer from '../images/legs-of-a-dancer.jpg';
+import Marcela from '../images/marcela.jpg';
+import ToweringIntoHeights from '../images/towering-into-heights-lost-in-clouds.jpg';
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <img src={NeckTat} />
+    <img src={InasLipoma} />
+    <img src={LegsOfADancer} />
+    <img src={FlorenciasLegs} />
+    <img src={ToweringIntoHeights} />
+    <img src={Marcela} />
   </Layout>
 )
 
